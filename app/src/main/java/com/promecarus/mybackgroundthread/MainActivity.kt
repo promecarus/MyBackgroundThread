34e4bb94ac79af880a1f978fcbc7fe370bc166bd
promecarus <haikalslipi@gmail.com>
2023-09-06 22:25:12 +0700

    part 01

--- app/src/main/java/com/promecarus/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/promecarus/mybackgroundthread/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.promecarus.mybackgroundthread
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+
+        findViewById<Button>(R.id.btn_start).setOnClickListener {
+            try {
+                for (i in 0..10) {
+                    Thread.sleep(500)
+                    val percentage = i * 10
+                    if (percentage == 100) {
+                        tvStatus.setText(R.string.task_completed)
+                    } else {
+                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                    }
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
